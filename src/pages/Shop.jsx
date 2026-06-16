039aa24d3e76eb6e3c493a0d780c6f6eb9b9545a
Isah Muhammad <alhajimuhammadisah@gmail.com>
2026-06-16 07:49:23 +0100

    connected to fakeapistore

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,65 +1,9 @@
 import ProductCard from "../components/ProductCard"
 function Shop() {
+    const products = fetch('https://fakestoreapi.com/products')
+     .then(response => response.json());
   // Temporary static data (later you will replace with FakeStore API)
-  const products = [
-    {
-      id: 1,
-      title: "Classic Minimalist Watch",
-      price: 120,
-      image:
-        "https://images.unsplash.com/photo-1523275335684-37898b6baf30?auto=format&fit=crop&q=80&w=800",
-    },
-    {
-      id: 2,
-      title: "Premium Wireless Headphones",
-      price: 299,
-      image:
-        "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?auto=format&fit=crop&q=80&w=800",
-    },
-    {
-      id: 3,
-      title: "Vintage Film Camera",
-      price: 450,
-      image:
-        "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?auto=format&fit=crop&q=80&w=800",
-    },
-    {
-      id: 4,
-      title: "Running Shoes Pro",
-      price: 185,
-      image:
-        "https://images.unsplash.com/photo-1491553895911-0055eca6402d?auto=format&fit=crop&q=80&w=800",
-    },
-    {
-      id: 5,
-      title: "Luxury Leather Bag",
-      price: 550,
-      image:
-        "https://images.unsplash.com/photo-1584917865442-de89df76afd3?auto=format&fit=crop&q=80&w=800",
-    },
-    {
-      id: 6,
-      title: "Stylish Sunglasses",
-      price: 75,
-      image:
-        "https://images.unsplash.com/photo-1572635196237-14b3f281503f?auto=format&fit=crop&q=80&w=800",
-    },
-    {
-      id: 7,
-      title: "Red Sports Sneakers",
-      price: 150,
-      image:
-        "https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&q=80&w=800",
-    },
-    {
-      id: 8,
-      title: "Wireless Earbuds",
-      price: 129,
-      image:
-        "https://images.unsplash.com/photo-1524678606370-a47ad25cb82a?auto=format&fit=crop&q=80&w=800",
-    },
-  ]
-
+  
   return (
     <div className="min-h-screen py-12">
       <div className="max-w-7xl mx-auto px-4">
